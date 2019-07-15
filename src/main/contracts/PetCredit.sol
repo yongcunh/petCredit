e0aefce9dc5df76e73127c84533a5a77b1f3fd68
charles chou <chouex@users.noreply.github.com>
2019-07-16 01:34:27 +0800

    update amdb version

--- src/main/contracts/PetCredit.sol
+++ src/main/contracts/PetCredit.sol
@@ -1,19 +1,25 @@
 pragma solidity ^0.4.25;
+import "./Table.sol";
  
 
 contract PetCredit {
+    event CreateResult(int count);
+    event InsertResult(int count);
+    event UpdateResult(int count);
+    event RemoveResult(int count);
 
-    struct Pet {
-        //uint id;  
-        string name;  
-        string cate;  
-        uint bdate;  
-        uint price;  
-        string desc;  
-        bool active;  
-        string extend;  
-        address owner;
-    } 
+
+    // struct Pet {
+    //     //int id;  
+    //     string name;  
+    //     string cate;  
+    //     int bdate;  
+    //     int price;  
+    //     string desc;  
+    //     bool active;  
+    //     string extend;  
+    //     address owner;
+    // } 
 
 //DEBUG
 string public message;
@@ -29,73 +35,176 @@ string public message;
  address private admin;
  
  //pet store
-  Pet[] public lists;
-        uint256 public salesCount;  
+  // Pet[] public lists;
+        int public salesCount;  
      
      //bank
-    mapping (address => uint256) private balances; 
+    mapping (address => int) private balances; 
     
     constructor() public { 
         admin = msg.sender;
         balances[admin]=10000;
+
+        TableFactory tf = TableFactory(0x1001);  // TableFactory's address is fixed at 0x1001
+        // To create a table t_test3. Table's key_field as name. Table's value_field as item_id and item_name.
+        // key_field indicates the row that AMDB's primary key value_field represents in the table. The row can be multiple and spearated by commas.
+        int count = tf.createTable("t_test3", "idx", "id,name,cate,bdate,price,desc,active,extend,owner");
+        // emit CreateResult(count);
+
     }
 
- 
- //get all pet in store
-    function getCount() public view returns (uint256) { 
-       return lists.length ;
+// inquiry data
+    function select(string memory name) public constant returns(bytes32[], int[], bytes32[]){
+        TableFactory tf = TableFactory(0x1001);
+        Table table = tf.openTable("t_test3");
+
+        // If the condition is empty, it means no filtering. You can use conditional filtering as needed.
+        Condition condition = table.newCondition();
+
+        Entries entries = table.select("name", condition);
+        bytes32[] memory user_name_bytes_list = new bytes32[](uint256(entries.size()));
+        int[] memory item_id_list = new int[](uint256(entries.size()));
+        bytes32[] memory item_name_bytes_list = new bytes32[](uint256(entries.size()));
+
+        for(int i=0; i<entries.size(); ++i) {
+            Entry entry = entries.get(i);
+
+            user_name_bytes_list[uint256(i)] = entry.getBytes32("id");
+            item_id_list[uint256(i)] = entry.getInt("price");
+            item_name_bytes_list[uint256(i)] = entry.getBytes32("cate");
+        }
+
+        return (user_name_bytes_list, item_id_list, item_name_bytes_list);
     }
-    
+
     //add 1 new pet
-    function add(  string memory name, string memory cate,  uint bdate, uint price, string memory desc, bool active, string memory extend) public returns (uint)    {
-      
-     lists.push(Pet(name,cate,bdate,price,desc,active,extend,msg.sender)); 
-       return lists.length-1;
+    function add(string memory id,string memory name, string memory cate,  int bdate, int price, string memory desc, bool active, string memory extend) public returns (int)    {
+       
+       TableFactory tf = TableFactory(0x1001);
+        Table table = tf.openTable("t_test3");
+
+        Entry entry = table.newEntry();
+
+        entry.set("id", id);
+        entry.set("name", name);
+        entry.set("cate", cate);
+        entry.set("bdate", bdate);
+        entry.set("price", price);
+        entry.set("desc", desc);
+        entry.set("active", active);
+        entry.set("extend", extend);
+        entry.set("owner", msg.sender);  
+
+        int count = table.insert("name", entry);
+// salesCount++;
+         emit InsertResult(count);
+        return count;
+
+//      // lists.push(Pet(name,cate,bdate,price,desc,active,extend,msg.sender)); 
+//      //   return lists.length-1;
     } 
      
+ 
+ //get all pet in store
+    function getCount() public constant returns (int) { 
+        // return salesCount;
+      TableFactory tf = TableFactory(0x1001);
+        Table table = tf.openTable("t_test3");
+
+        // If the condition is empty, it means no filtering. You can use conditional filtering as needed.
+        Condition condition = table.newCondition();
+
+         Entries entries = table.select("name", condition); 
+
+       return entries.size();
+    }
+    
+
+    function get(string memory id) public returns(Entry){ 
+         TableFactory tf = TableFactory(0x1001);
+        Table table = tf.openTable("t_test3");
+
+        // If the condition is empty, it means no filtering. You can use conditional filtering as needed.
+        Condition condition = table.newCondition();
+
+        Entries entries = table.select("name", condition); 
+
+            Entry entry = entries.get(0);
+            return entry;
+    }
+    function _update(string memory id,Entry entry) private returns(Entry){ 
+         TableFactory tf = TableFactory(0x1001);
+        Table table = tf.openTable("t_test3");
+
+        // If the condition is empty, it means no filtering. You can use conditional filtering as needed.
+        Condition condition = table.newCondition();
+
+        Entries entries = table.select("name", condition); 
+
+        table.update(id,entry, condition); 
+    }
      //buy active pet
-    function buy(uint i) public      { 
-        if( balances[msg.sender]<lists[i].price)throw;
-        if(!lists[i].active)throw;
-           balances[msg.sender]-=lists[i].price;
-           balances[lists[i].owner]+=lists[i].price;
-           lists[i].active=false;
-           lists[i].owner=msg.sender;
+    function buy(string memory id) public{  
+            Entry entry = get(id);
+
+        require( balances[msg.sender]>=entry.getInt("price"));
+        if(!entry.getBoolean("active"))throw;
+           balances[msg.sender]-=entry.getInt("price");
+           balances[entry.getAddress("owner")]+=entry.getInt("price");
+        entry.set("active", false); 
+        entry.set("owner", msg.sender);   
+        _update(id,entry); 
            salesCount++;
     }
      
      //seller or admin set their pet active
-    function setActive(uint i,bool active) public      { 
-        if( lists[i].owner!=msg.sender||admin!=msg.sender)throw;
-           lists[i].active=active; 
+    function setActive(string memory id,bool active) public      { 
+        Entry entry = get(id);
+
+        require( entry.getAddress("owner")==msg.sender||admin==msg.sender);
+        entry.set("active", active); 
+        _update(id,entry);  
     }
+      
      
-    function get(uint i) public view returns ( string memory , string memory ,  uint , uint , string memory,bool,address  ) {   
-         return (lists[i].name,lists[i].cate,lists[i].bdate,lists[i].price,lists[i].desc,lists[i].active,lists[i].owner); 
-    }
-     
-     //avg, sum
-    function priceState( ) public view returns (uint ,uint   ) { 
-        if(!isAdmin())throw;
-        uint tmp=0;
-            for (uint8 prop = 0; prop < lists.length; prop++)
-            (tmp+=lists[prop].price ) ; 
-            return (tmp/lists.length,tmp);
-    }
+  //    //avg, sum
+  //   function priceState( ) public constant returns  ( int   ) { 
+  //       require(isAdmin());
+  //       int tmp=0;
+  //      TableFactory tf = TableFactory(0x1001);
+  //       Table table = tf.openTable("t_test3");
+
+  //       // If the condition is empty, it means no filtering. You can use conditional filtering as needed.
+  //       Condition condition = table.newCondition();
+
+  //        Entries entries = table.select("name", condition); 
+  //        return entries.size();
+  // // for(int i=0; i<entries.size(); ++i) {
+  // //           Entry entry = entries.get(i);
+  // //   tmp+=entry.getInt("price"); 
+
+  // //       } 
+  // //           return (tmp);
+  //   }
      //see how rich someone is
-    function getCredit(address owner ) public view returns (uint   ) { 
+    function getCredit(address owner ) public constant returns (int   ) { 
      return balances[owner];
     }
     //admin charge control
-    function addCredit(address owner1,uint credit ) public   returns (uint   ) { 
-        if(!isAdmin())throw;
+    function addCredit(address owner1,int credit ) public   returns (int   ) { 
+      require(isAdmin());
         balances[owner1]+=credit;
      return balances[owner1] ;
     }
     //admin force set pet owner
-    function setOwner(uint i,address owner ) public  { 
-        if(!isAdmin())throw;
-      lists[i].owner=owner;
+    function setOwner(string memory id,address owner ) public  { 
+       require(isAdmin());
+           Entry entry = get(id);
+
+        require( entry.getAddress("owner")==msg.sender||admin==msg.sender);
+        entry.set("owner", owner); 
+        _update(id,entry);  
+ 
     }
     
  
--- src/main/contracts/Table.sol
+++ src/main/contracts/Table.sol
@@ -1,55 +1,68 @@
+//https://fisco-bcos-documentation.readthedocs.io/en/latest/docs/manual/smart_contract.html
+pragma solidity ^0.4.24;
+
 contract TableFactory {
-    function openTable(string) public constant returns (Table); //open table
-    function createTable(string,string,string) public constant returns(int); //create table
+    function openTable(string) public constant returns (Table);  // open table
+    function createTable(string,string,string) public returns(int);  // create table
 }
 
-//select condition
+// inquiry conditions
 contract Condition {
-    function EQ(string, int);
-    function EQ(string, string);
-    
-    function NE(string, int);
-    function NE(string, string);
+    //equal to
+    function EQ(string, int) public;
+    function EQ(string, string) public;
 
-    function GT(string, int);
-    function GE(string, int);
-    
-    function LT(string, int);
-    function LE(string, int);
-    
-    function limit(int);
-    function limit(int, int);
+    //unequal to
+    function NE(string, int) public;
+    function NE(string, string)  public;
+
+    //greater than
+    function GT(string, int) public;
+    //greater than or equal to
+    function GE(string, int) public;
+
+    //smaller than
+    function LT(string, int) public;
+    //smaller than or equal to
+    function LE(string, int) public;
+
+    //limit the number of return record
+    function limit(int) public;
+    function limit(int, int) public;
 }
 
-//one record 
+// single entry data record
 contract Entry {
     function getInt(string) public constant returns(int);
+    function getBoolean(string) public constant returns(bool);
     function getAddress(string) public constant returns(address);
     function getBytes64(string) public constant returns(byte[64]);
     function getBytes32(string) public constant returns(bytes32);
-    function getString(string) public constant returns(string);
-    
+
     function set(string, int) public;
+    function set(string, bool) public;
     function set(string, string) public;
+    function set(string, address) public;
 }
 
-//record sets
+// data record set
 contract Entries {
     function get(int) public constant returns(Entry);
     function size() public constant returns(int);
 }
 
-//Table main contract
+// Table main type
 contract Table {
-    //select api
+    // select interface
     function select(string, Condition) public constant returns(Entries);
-    //insert api
+    //function select( Condition) public constant returns(Entries);
+    // insert interface
     function insert(string, Entry) public returns(int);
-    //update api
+    // update interface
     function update(string, Entry, Condition) public returns(int);
-    //remove api
+    // remove interface
     function remove(string, Condition) public returns(int);
-    
+
     function newEntry() public constant returns(Entry);
     function newCondition() public constant returns(Condition);
 }
